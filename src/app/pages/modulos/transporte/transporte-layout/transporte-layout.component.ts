8bed7b6d27ac99f4d3fdcdf670e280661e9053a2
victor <vicjosneros@gmail>
2024-09-30 14:50:58 -0600

    feat: corrigiendo routes

--- src/app/pages/modulos/transporte/transporte-layout/transporte-layout.component.ts
+++ src/app/pages/modulos/transporte/transporte-layout/transporte-layout.component.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from "@angular/material/icon";
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-transporte-layout',
   standalone: true,
-  imports: [MatIconModule, CommonModule],
+  imports: [RouterOutlet, CommonModule, ReactiveFormsModule, MatIconModule, RouterLink],
   templateUrl: './transporte-layout.component.html',
   styleUrl: './transporte-layout.component.css'
 })
