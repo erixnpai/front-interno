c2bdf6adcb480f71049615f238c307f5c4f62d13
Kevin García <garciakev525@gmail.com>
2024-10-02 16:49:12 -0600

    tabla de historial de transporte

--- src/app/pages/modulos/router_transporte.ts
+++ src/app/pages/modulos/router_transporte.ts
@@ -12,6 +12,11 @@ export  const RouterTransporte: Routes = [
         title: 'Solicitar Transporte',
         loadComponent: () => import('./transporte/solicitar-transporte/solicitar-transporte.component')
     },
+    {
+        path: 'historial',
+        title: 'Historial Transporte',
+        loadComponent: () => import('./transporte/historial-transporte/historial-transporte.component')
+    },
 ];
 
 export default RouterTransporte;
---
+++ src/app/pages/modulos/transporte/historial-transporte/historial-transporte.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-historial-transporte',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './historial-transporte.component.html',
+  styleUrl: './historial-transporte.component.css'
+})
+export default class HistorialTransporteComponent {
+
+}
