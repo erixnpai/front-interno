2368d7268d132e5c5ebf508cdcc4776d8ab2cadb
Kevin García <garciakev525@gmail.com>
2024-10-04 16:58:48 -0600

    componente de solicitar transpote

---
+++ src/app/pages/modulos/transporte/solicitudes-transporte/solicitudes-transporte.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-solicitudes-transporte',
+  standalone: true,
+  imports: [MatIconModule],
+  templateUrl: './solicitudes-transporte.component.html',
+  styleUrl: './solicitudes-transporte.component.css'
+})
+export default class SolicitudesTransporteComponent {
+
+}
