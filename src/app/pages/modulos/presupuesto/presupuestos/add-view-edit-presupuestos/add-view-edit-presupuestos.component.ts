02f8b78382782e0a22bb85db55d237495e77bd70
erinxpai <erianhernandez97@gmail.com>
2024-10-11 16:46:24 -0600

    instalando ng select

--- src/app/pages/modulos/presupuesto/presupuestos/add-view-edit-presupuestos/add-view-edit-presupuestos.component.ts
+++ src/app/pages/modulos/presupuesto/presupuestos/add-view-edit-presupuestos/add-view-edit-presupuestos.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Inject } from '@angular/core';
 import { ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatDialogContent, MatDialogActions, MatDialogClose, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { PresupuestoServices } from '../../../../../Services/Presupuesto-servs/Presupuesto.service';
 
 @Component({
   selector: 'app-add-view-edit-presupuestos',
@@ -15,6 +16,7 @@ export class AddViewEditPresupuestosComponent {
   formulario: FormGroup<any>;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
+  private srvPresupuesto: PresupuestoServices,
   private dialogoRef: MatDialogRef<AddViewEditPresupuestosComponent>
 ) {
     this.formulario = new FormGroup({
@@ -22,9 +24,15 @@ export class AddViewEditPresupuestosComponent {
       ConceptoNombre: new FormControl('', [Validators.required]),
       AnioAplicado: new FormControl('', [Validators.required]),
       MontoAsignadoOrigen: new FormControl('', [Validators.required]),
+      MontoEjecutado: new FormControl('', [Validators.required]),
+      PorcentajeUsado: new FormControl('', [Validators.required]),
+      EstadoPresupuesto: new FormControl('', [Validators.required]),
     })
   }
   cerraDialogo() {
     this.dialogoRef.close();
+  }
+  async cargarEstadoPresupuesto() {
+    // code here
   }
 }
