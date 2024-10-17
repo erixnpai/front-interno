2307894af687d07756b7e513d84e54b8f7aad14d
ibonilla <ibonillavillarreal@yahoo.com>
2024-10-17 11:27:20 -0600

    Actualizando node v20.18.0

---
+++ src/app/pages/modulos/presupuesto/catalogos-cuentas/catalogos-cuentas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogos-cuentas',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalogos-cuentas.component.html',
+  styleUrl: './catalogos-cuentas.component.css'
+})
+export class CatalogosCuentasComponent {
+
+}
