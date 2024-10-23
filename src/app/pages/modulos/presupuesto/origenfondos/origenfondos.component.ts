09e6cab14a0f4567d7d3c8b6b0889bd0125d94ed
erixnpai <erianhernandez97@gmail.com>
2024-10-23 16:04:34 -0600

    diseño terminado del dashboard

--- src/app/pages/modulos/presupuesto/origenfondos/origenfondos.component.ts
+++ src/app/pages/modulos/presupuesto/origenfondos/origenfondos.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-origenfondos',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule, MatIconModule],
   templateUrl: './origenfondos.component.html',
   styleUrl: './origenfondos.component.css'
 })
